1ec0b0ea50a1b721962d90c259a9511249ff3698
wiz <wizneo1@gmail.com>
2015-03-15 17:07:21 +0900

    remove ohjic

--- views/ohjic_tpl.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="http://namune.net/test.css">
-<div id='asdf'>
-	<img src="http://static.naver.net/www/mobile/edit/2014/0306/mobile_184623301662.jpg" width="90" height="51" alt="네이버" class="lg_m" />
-	<img src="http://static.naver.net/www/mobile/edit/2014/0306/mobile_184623301662.jpg" width="90" height="51" alt="네이버" class="lg_m" />
-	<tpl for=''>
-	<h1 style='background:blue;color:white;'>{title}</h1>
-	</tpl>
-</div>
-
