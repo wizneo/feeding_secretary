724d42a02399e65717272e05d91ca9fd380553e2
wiz <wizneo1@gmail.com>
2016-09-09 13:01:03 +0900

    test

--- controllers/feedingmilktobaby.php
+++ controllers/feedingmilktobaby.php
@@ -2,7 +2,8 @@
 class Feedingmilktobaby extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
- 		$this->load->model('Message_model','message',true);
+ 		$this->load->model('Fd_message_model','message',true);
+ 		$this->load->model('Fd_user_model','user',true);
 // 		$this->load->helper('url');
 	}
 
@@ -17,9 +18,12 @@ class Feedingmilktobaby extends CI_Controller {
 		echo $output_data;
 		//$this->load->view('memo_index', $output_data);
 	}
-
+	
+	private function getInput() {
+		return json_decode(file_get_contents('php://input'));
+	}
 	public function message() {
-		$input_data = json_decode(file_get_contents('php://input'));
+		$input_data = $this->getInput();
 		$user_key = $input_data->user_key;
 		$type = $input_data->type;
 		$content = $input_data->content;
@@ -35,6 +39,15 @@ class Feedingmilktobaby extends CI_Controller {
 		);
 		$this->message->insert_message($input_data);	
 		echo json_encode($output_data);
+	}
+	
+	public function friend() {
+		$input_data = $this->getInput();
+		$user_key = $input_data->user_key;
+		$input_data = array(
+			'user_key' => $user_key
+		);
+		$this->user->insert_user($input_data);
 	}
 
 	public function save() {
