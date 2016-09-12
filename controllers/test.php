28edc4b098352925cfeef3ff7bf4937ff5b01def
wiz <wizneo1@gmail.com>
2016-09-12 12:49:25 +0900

    test

--- controllers/test.php
+++ controllers/test.php
@@ -3,7 +3,7 @@ class Test extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 //		$this->load->model('Memo_model','memo',true);
-//		$this->load->helper('url');
+		$this->load->helper('url');
 	}
 
 
