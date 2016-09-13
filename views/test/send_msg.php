62a87607a2ed2e71887974af27c6441139dd4d61
wiz <wizneo1@gmail.com>
2016-09-13 12:50:19 +0900

    test

--- views/test/send_msg.php
+++ views/test/send_msg.php
@@ -5,28 +5,28 @@
 <head>
 <script type="text/javascript" src="<?=base_url();?>js/jquery-3.1.0.min.js" />
 <script type="text/javascript">
-function sendMsg() {
-	$.ajax({
-	    url: '/feedingmilktobaby/message/',
-	    type: 'POST',
-	    dataTyle: 'json',
-	    data: {
-		    user_key : 'test_user_key',
-		    type: 'text',
-		    content: $("#msg").val();
-	    }
-	    success: function(result) {
-	        $("#response").val(result);
-	    }
-	});
-}
+var controller = {
+	sendMsg : function () {
+		$.ajax({
+		    url: '/feedingmilktobaby/message/',
+		    type: 'POST',
+		    dataTyle: 'json',
+		    data: {
+			    user_key : 'test_user_key',
+			    type: 'text',
+			    content: $("#msg").val();
+		    }
+		    success: function(result) {
+		        $("#response").val(result);
+		    }
+		});
+	}
+};
 </script>
 </head>
 <body>
-	<textarea id="msg">
-	</textarea>
-	<input type="button" value="send msg" onclick="sendMsg()" />
-	<textarea id="response">
-	</textarea>
+	<textarea id="msg"></textarea>
+	<input type="button" value="send msg" onclick="controller.sendMsg()" />
+	<textarea id="response"></textarea>
 </body>
 </html>
