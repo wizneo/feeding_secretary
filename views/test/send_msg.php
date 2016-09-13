b8bdd5fa1ec5380e7deb18f039cff3fe9d3760ec
wiz <wizneo1@gmail.com>
2016-09-13 12:44:46 +0900

    test

--- views/test/send_msg.php
+++ views/test/send_msg.php
@@ -6,7 +6,14 @@
 <script type="text/javascript" src="<?=base_url();?>js/jquery-3.1.0.min.js" />
 <script type="text/javascript">
 function sendMsg() {
-	
+	$.ajax({
+	    url: '/feedingmilktobaby/message/',
+	    type: 'POST',
+	    dataTyle: 'json',
+	    success: function(result) {
+	        $("#response").val(result);
+	    }
+	});
 }
 </script>
 </head>
@@ -14,5 +21,7 @@ function sendMsg() {
 	<textarea id="msg">
 	</textarea>
 	<input type="button" value="send msg" onclick="sendMsg()" />
+	<textarea id="response">
+	</textarea>
 </body>
 </html>
