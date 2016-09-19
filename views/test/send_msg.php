9ea0817dbc07b2db6e4e347aed743650fefe346e
wiz <wizneo1@gmail.com>
2016-09-19 12:57:43 +0900

    test

--- views/test/send_msg.php
+++ views/test/send_msg.php
@@ -11,11 +11,7 @@ var controller = {
 		    url: '/feedingmilktobaby/message/',
 		    method: 'POST',
 		    dataTyle: 'json',
-		    data: {
-			    user_key : 'test_user_key',
-			    type: 'text',
-			    content: $("#msg").val()
-		    },
+		    data: $("#msg_frm").serialize(),
 		    contentType : 'application/json; charset=utf-8',
 		    success: function(result) {
 		        $("#response").html(result);
@@ -26,7 +22,11 @@ var controller = {
 </script>
 </head>
 <body>
-	<textarea id="msg"></textarea>
+	<form id="msg_frm">
+	<textarea name="content" id="content"></textarea>
+	<input type="hidden" name="user_key" value= "test_user_key"/>
+	<input type="hidden" name="type" value= "text"/>
+	</form>
 	<input type="button" value="send msg" onclick="controller.sendMsg()" />
 	<div id="response"></div>
 </body>
