208167f366286de474e3d148dc5aa1c43f902e40
wiz <wizneo1@gmail.com>
2016-09-19 12:44:33 +0900

    test

--- views/test/send_msg.php
+++ views/test/send_msg.php
@@ -5,25 +5,24 @@
 <head>
 <script type="text/javascript" src="<?=base_url();?>js/jquery-3.1.0.min.js"></script>
 <script type="text/javascript">
-var aaaaa = "er";
-//var controller = {
-//	sendMsg : function () {
-//		$.ajax({
-//		    url: '/feedingmilktobaby/message/',
-//		    method: 'POST',
-//		    dataTyle: 'json',
-//		    data: {
-//			    user_key : 'test_user_key',
-//			    type: 'text',
-//			    content: $("#msg").val()
-//		    },
-//		    contentType : 'application/json; charset=utf-8',
-//		    success: function(result) {
-//		        $("#response").val(result);
-//		    }
-//		});
-//	}
-//};
+var controller = {
+	sendMsg : function () {
+		$.ajax({
+		    url: '/feedingmilktobaby/message/',
+		    method: 'POST',
+		    dataTyle: 'json',
+		    data: {
+			    user_key : 'test_user_key',
+			    type: 'text',
+			    content: $("#msg").val()
+		    },
+		    contentType : 'application/json; charset=utf-8',
+		    success: function(result) {
+		        $("#response").val(result);
+		    }
+		});
+	}
+};
 </script>
 </head>
 <body>
