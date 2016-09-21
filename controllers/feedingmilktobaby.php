dd5b17f3e02a8b1eccb5c4fd294f1fd18f84a721
wiz <wizneo1@gmail.com>
2016-09-21 12:42:53 +0900

    test

--- controllers/feedingmilktobaby.php
+++ controllers/feedingmilktobaby.php
@@ -79,7 +79,7 @@ class Feedingmilktobaby extends CI_Controller {
 		// 메세지 분석 
 		$analized_msg = $this->anaylize_message($content);
 		
-		if ($$analized_msg->result == 'FAIL') {
+		if ($analized_msg->result == 'FAIL') {
 			$out_message = "인식할 수 없는 형태의 명령입니다. 명령의 예를 보려면 '야' 또는 '수유비서'라고 불러주세요.";
 		}
 		// 수유 기록  저장
