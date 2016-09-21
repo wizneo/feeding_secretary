d2ce2b58c92d3f719b7db62f75294fc0d9ecc7da
wiz <wizneo1@gmail.com>
2016-09-22 01:06:10 +0900

    test

--- models/fd_message_model.php
+++ models/fd_message_model.php
@@ -27,6 +27,7 @@ class Fd_message_model extends CI_Model {
 	}
 	
 	public function cancel_feeding_hst($user_key) {
+		echo $user_key.PHP_EOL;
 		$sql = "select no, msg_no from fd_feeding_hst where user_key = ? order by no desc limit 1";
 		$query = $this->db->query($sql, array($user_key));
 		$result_arr = $query->result();
