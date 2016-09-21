6344199b6332b3163b13507b5019be243888c21a
wiz <wizneo1@gmail.com>
2016-09-21 23:01:47 +0900

    test

--- controllers/test.php
+++ controllers/test.php
@@ -2,14 +2,13 @@
 class Test extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
-//		$this->load->model('Memo_model','memo',true);
+		$this->load->model('Fd_message_model','message',true);
 		$this->load->helper('url');
 	}
 
 
 	public function index() {
-		$date = date('Y-m-d', strtotime(date('Y-m-d').' +1 day'));
-		echo $date;
+		$this->message->get_today_total_amount('test_user_key');
 //		$output_data = array(
 //			'memo_list' => $this->memo->get_list(),
 //			);
@@ -19,28 +18,6 @@ class Test extends CI_Controller {
 	public function send_msg() {
 		$this->load->view('test/send_msg');
 	}
-
-	public function form() {
-		$this->load->view('memo_form');	
-	}
-
-	public function save() {
-		$contents = $this->input->post('contents');
-		$title = strlen($this->input->post('title')) == 0 ? substr($contents, 0, 50) : $this->input->post('title');
-		
-		$input_data  = array(
-			'title' => $title,
-			'contents' => $contents,
-			'del_yn' => 'N'
-			);
-
-		$this->memo->insert_memo($input_data);
-		redirect('/memo');
-	}
-
-	public function delete($num) {
-		$this->memo->delete_memo($num);
-		redirect('/memo');
-	}
+	
 }
 ?>
--- models/fd_message_model.php
+++ models/fd_message_model.php
@@ -6,6 +6,23 @@ class Fd_message_model extends CI_Model {
 		parent::__construct();
 	}
 
+	private function get_max_no($user_key) {
+		$this->db->where('user_key', $user_key);
+		$this->db->select_max('no');
+		$query = $this->db->get(self::$table_message);
+		$result_arr = $query->result();
+		return $result_arr[0]->no;
+	}
+	
+	public function get_today_total_amount($user_key) {
+		$sql = "SELECT sum(`amount`) as today_total_amount FROM `fd_feeding_hst` WHERE user_key = ? AND feeding_dtm BETWEEN ? AND ?";
+		$query = $this->db->query($sql, array($user_key, date('Y-m-d'), date('Y-m-d', strtotime(date('Y-m-d').' +1 day'))));
+		foreach ($query->result() as $row) {
+			echo $row->today_total_amount;
+		}
+		
+	}
+	
 	public function insert_message($data) {
 		$data['reg_dtm'] = date('Y-m-d H:i:s');
 		$this->db->insert(self::$table_message, $data);
@@ -15,14 +32,6 @@ class Fd_message_model extends CI_Model {
 		$data['reg_dtm'] = date('Y-m-d H:i:s');
 		$data['msg_no'] = $this->get_max_no($data['user_key']);
 		$this->db->insert(self::$table_feeding_hst, $data);
-	}
-	
-	private function get_max_no($user_key) {
-		$this->db->where('user_key', $user_key);
-		$this->db->select_max('no');
-		$query = $this->db->get(self::$table_message);
-		$result_arr = $query->result();
-		return $result_arr[0]->no;
 	}
 
 	public function get_list($user_key) {
